0d7d6d72e3758ba5e9db9e02d62d5b34a0300d9a
Nikhil Gupta <nguptaa183@gmail.com>
2023-11-09 20:13:38 -0500

    Assign part 3

--- try.py
+++ try.py
@@ -6,41 +6,50 @@ from torch.utils.data import DataLoader
 
 # Define VGG-11 architecture
 class VGG11(nn.Module):
-    def __init__(self, num_classes=36):
+    def __init__(self):
         super(VGG11, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+
+        self.convolutionlayers = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.Conv2d(512, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
-        self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
+
+        # fully connected linear layers
+        self.fclayers = nn.Sequential(
+            nn.Linear(12 * 7 * 7, 4096),
+            nn.ReLU(),
+            nn.Dropout2d(0.5),
             nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes)
+            nn.ReLU(),
+            nn.Dropout2d(0.5),
+            nn.Linear(4096, 36)
         )
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.convolutionlayers(x)
         x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        x = self.fclayers(x)
         return x
+
 
 # MNIST data loading and transformation
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
